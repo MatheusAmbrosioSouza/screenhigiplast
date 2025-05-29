645ad4a3aecb9092547b9eb3c8a99556f5d79282
mathambro <matheusambrosio6@gmail.com>
2025-05-28 21:31:44 -0300

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+//import Image from "next/image";
+//import styles from "./page.module.css";
+import * as React from "react";
+import {Box, Typography, /*Divider,*/ Container,} from "@mui/material"
+
+export default function Home() {
+  return (
+    <Box> 
+      <Container>
+        <Typography>
+
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
